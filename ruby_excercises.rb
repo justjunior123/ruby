f480afdca811f055acd266111bc65623df26e008
justjunior89 <38800554+justjunior89@users.noreply.github.com>
2018-07-05 12:58:59 -0700

    Update ruby_excercises.rb

--- ruby_excercises.rb
+++ ruby_excercises.rb
@@ -47,23 +47,6 @@ p "number: #{number}, array[index + 1]:#{array[index + 1]}"
   (index + 1) % 3 == 0 #@3 idx = 3 here is where the magic all happens.
 end
 empty_arr
-#*****************Extra Practice************************
-#"m1i2s1i2s1i2p1i" # mississippi 
-def find(string)
-  empty_str = string.slice!(0) # takes away the 'm'
-  current_num = 0
-  i = 0
-  until i == string.length #@4  i = 3 
-    if string[i] =~ /[0-9]/#@1 '1'
-      current_num = string[i].to_i#@3 current numb = 2
-    elsif string[i] =~ /[a-z]/#@4 's'
-      empty_str += (string[i] * current_num)
-    end
-    i += 1
-  end
-  empty_str
-end
- find("m1i2s1i2s1i2p1i")
 ################################****************************************************************************
 ############################
 def reverse_word(string)
